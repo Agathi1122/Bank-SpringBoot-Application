33bd8a77a03054bf26c81ab6246a9c176a1dc0f1
AGATHI <AGATHI@DESKTOP-9OBGLJQ>
2024-03-07 13:54:05 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@GetMapping("/demo")
+	String demomethod()
+	{
+		return "you are working in spring - boot demo app";
+	}
+}
